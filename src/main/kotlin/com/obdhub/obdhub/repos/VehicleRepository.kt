e31f8be7cb6e27b8df1f9d1a787f8c6dbce5fbc6
benmonty2001 <benMonty2001@gmail.com>
2024-01-04 10:50:28 -0600

    vishal's fixed branch get and post

--- src/main/kotlin/com/obdhub/obdhub/repos/VehicleRepository.kt
+++ src/main/kotlin/com/obdhub/obdhub/repos/VehicleRepository.kt
@@ -4,5 +4,4 @@ import com.obdhub.obdhub.domain.Vehicle
 import java.util.UUID
 import org.springframework.data.jpa.repository.JpaRepository
 
-
 interface VehicleRepository : JpaRepository<Vehicle, UUID>
