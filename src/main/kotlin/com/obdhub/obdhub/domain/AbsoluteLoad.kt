3f02b0dfc1cb851a69a8d7edda6e727177277648
Benjamin Montgomery <73138581+benMonty2001@users.noreply.github.com>
2024-01-15 15:21:37 -0600

    Update AbsoluteLoad.kt
    optimized imports ( removed import jakarta.persistence.* )

--- src/main/kotlin/com/obdhub/obdhub/domain/AbsoluteLoad.kt
+++ src/main/kotlin/com/obdhub/obdhub/domain/AbsoluteLoad.kt
@@ -1,9 +1,15 @@
 package com.obdhub.obdhub.domain
 
-import jakarta.persistence.*
-import org.hibernate.annotations.GenericGenerator
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import java.time.OffsetDateTime
-import java.util.*
+import java.util.UUID
+import org.hibernate.annotations.GenericGenerator
 
 
 @Entity
